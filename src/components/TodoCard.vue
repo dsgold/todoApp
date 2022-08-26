a342a3c38b6447a750a2ab6db052dd75bdbdc425
DSgold <72311610+dsgold@users.noreply.github.com>
2022-08-27 02:16:52 +0300

    Create todo card and todo section

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="flex flex-col border border-black w-72 h-72">
-    <div class="flex-none border-black border-b">
+
+  <div class="flex flex-col w-72 h-72 rounded-lg shadow shadow-white drop-shadow-sm">
+    <div class="flex-none border-gray-400 border-b">
       <div class="flex p-1">
         <span class="left-0">Card tags</span>
         <div class="flex ml-auto">
@@ -11,21 +12,23 @@
       </div>
     </div>
     <div class="grow p-5 al">
-      {{this.cardName}}
+      {{ this.cardName }}
       <br>
-      {{this.cardText}}
+      {{ this.cardText }}
     </div>
-    <div class="flex-none border-black border-t">
-      <div v-if="this.cardCheckList.length > 1" class="flex flex-row items-center p-1">
+    <div v-if="this.cardCheckList.length > 1" class="flex-none border-gray-400 border-t ">
+      <div class="flex flex-row items-center p-1">
         <BaseIcon name="checked"/>
-        <span class="pl-1">{{this.cardCheckList[0]}}/{{this.cardCheckList[1]}}</span>
+        <span class="pl-1">{{ this.cardCheckList[0] }}/{{ this.cardCheckList[1] }}</span>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
 import BaseIcon from "./BaseIcon.vue";
+
 export default {
   name: "TodoCard",
   components: {
